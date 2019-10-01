8a5fed7b21b8c58b1330d9eeaafa68c210dd1ca3
James Cahours <jwcahours@gmail.com>
2019-09-30 23:30:40 -0500

    added webpack define plugin

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,8 +4,8 @@ const AboutPage = () => (
   <div>
     <h2>About</h2>
     <p>
-      This app uses React, Redux, React Router, and many other helpful
-      libraries.
+      This app uses React, Redux, React Router, Redux-Thunk, and many other
+      helpful libraries.
     </p>
   </div>
 );
